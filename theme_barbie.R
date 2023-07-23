05f541d273c048bfa62b945aa646c294bc13bd23
Matthew B. Jané <52077481+MatthewBJane@users.noreply.github.com>
2023-07-23 12:11:35 -0400

    Update theme_barbie.R

--- theme_barbie.R
+++ theme_barbie.R
@@ -1,27 +1,30 @@
 require('ggplot2')
 
-theme_barbie <- function(legend.position = "none"){
-  ggplot2::theme(panel.grid.minor = element_blank(),
-        panel.grid.major = element_blank(),
-        title = element_text(color = '#a62675ff'),
-        panel.background = element_rect(fill = '#fdf6faff'),
-        panel.border = element_rect(fill = NA, color = '#d74ea2ff',linewidth=1.2),
-        axis.title = element_text(size=14, color = '#a62675ff'),
-        axis.text.x = element_text(size=12, color = '#a62675ff'),
-        axis.text.y = element_text(size=12, color = '#a62675ff'),
-        axis.ticks = element_line(color = '#d74ea2ff',linewidth=1),
-        legend.background = element_rect(fill = '#fdf6faff', color = NA),,
-        legend.text = element_text(color = '#a62675ff')
-       )
-}
-
-
-scale_fill_barbie <- ggplot2::scale_fill_gradient(low = '#eeb4d7ff', high = '#bf2986ff')
-
-
 text_color_barbie    <- '#a62675ff'
 panel_color_barbie   <- '#fdf6faff'
 border_color_barbie  <- '#d74ea2ff'
 light_color_barbie   <- '#eeb4d7ff'
 medium_color_barbie  <- '#d74ea2ff'
 dark_color_barbie    <- '#bf2986ff'
+
+
+theme_barbie <- function(){
+  ggplot2::theme(
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        text = element_text(color = text_color_barbie)
+        panel.background = element_rect(fill = panel_color_barbie),
+        panel.border = element_rect(fill = NA, color = border_color_barbie,linewidth=1.2),
+        axis.title = element_text(size=14),
+        axis.text = element_text(size=12),
+        axis.ticks = element_line(color = border_color_barbie,linewidth=1),
+        legend.background = element_rect(fill = panel_color_barbie, color = NA)
+       )
+}
+
+
+scale_fill_barbie <- ggplot2::scale_fill_gradient(low = '#eeb4d7ff', high = '#bf2986ff')
+scale_color_barbie <- ggplot2::scale_color_gradient(low = '#eeb4d7ff', high = '#bf2986ff')
+
+
+
