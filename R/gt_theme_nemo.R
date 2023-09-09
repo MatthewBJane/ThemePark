4c6019d92d1ac9d09e184290f0e9496573fdcfa4
Christopher Kenny <ctk44@cornell.edu>
2023-09-09 14:22:06 -0400

    clean up grey lines; fix gt_everywhere

--- R/gt_theme_nemo.R
+++ R/gt_theme_nemo.R
@@ -67,6 +67,10 @@ gt_theme_nemo <- function(data, ...) {
       style = gt::cell_borders(sides = c('top'), color = 'black'),
       location = gt::cells_grand_summary()
     ) |>
+    gt::tab_style(
+      style = gt::cell_borders(sides = c('right'), color = 'black'),
+      location = gt::cells_stub_grand_summary()
+    ) |>
     gt::opt_table_outline(color = 'black', width = gt::px(4)) |>
     # rest of borders
     gt::tab_options(
--- R/utils.R
+++ R/utils.R
@@ -91,9 +91,15 @@ gt_cells_everywhere <- function() {
     gt::cells_body(),
     gt::cells_column_labels(),
     gt::cells_column_spanners(),
-    gt::cells_title(),
+    gt::cells_footnotes(),
+    gt::cells_grand_summary(),
+    gt::cells_row_groups(),
+    gt::cells_source_notes(),
+    gt::cells_stub(),
+    gt::cells_stub_grand_summary(),
+    #gt::cells_summary(), # breaks if no summary
     gt::cells_stubhead(),
-    gt::cells_stub()
+    gt::cells_title()
   )
 }
 
