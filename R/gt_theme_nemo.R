c2786d29c5b8578d9d2c4326e6efbcbc4d908acb
Christopher Kenny <ctk44@cornell.edu>
2023-09-08 22:21:48 -0400

    nemo stripes

---
+++ R/gt_theme_nemo.R
@@ -0,0 +1,45 @@
+#' Finding Nemo Inspired Table Theme
+#'
+#' @param data a table of class `gt_tbl`
+#' @param ... additional arguments to pass to `gt::tab_options()`
+#'
+#' @return a `gt_tbl`
+#' @export
+#'
+#' @examples
+#' gt::exibble |>
+#'   gt::gt() |>
+#'   gt_theme_nemo()
+gt_theme_nemo <- function(data, ...) {
+
+  data |>
+    # first update the font
+    gt::opt_table_font(font = gt::google_font("Bowlby One SC")) |>
+    # set background colors
+    gt::opt_row_striping() |>
+    gt::opt_table_outline(color = 'black', width = gt::px(4)) |>
+    # rest of borders
+    gt::tab_options(
+      # top colors, generally black
+      table.border.top.color = 'black',
+      column_labels.border.top.color = 'black',
+      row_group.border.top.color = 'black',
+      summary_row.border.color = 'black',
+      # bottom colors, generally black
+      table.border.bottom.color = 'black',
+      heading.border.bottom.color = 'black',
+      column_labels.border.bottom.color = 'black',
+      row_group.border.bottom.color = 'black',
+      table_body.border.bottom.color = 'black',
+      # make this pink so that sourcenotes and footnotes blend
+      # footnotes.border.bottom.color = unname(barbie_theme_colors['medium']),
+      source_notes.border.bottom.color = 'black',
+      # # backgrounds
+      # source_notes.background.color = unname(barbie_theme_colors['medium']),
+      # footnotes.background.color = unname(barbie_theme_colors['medium']),
+      row.striping.background_color = unname(nemo_theme_colors['medium']),
+      # any other options passed by users
+      ...
+    )
+
+}
